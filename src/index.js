d8cb031da7dc2630f6d6bafc6d7b6e5572ece3cf
iamwatchdogs <shamith301102@gmail.com>
2023-12-04 23:51:01 +0530

    Added the imported components

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Imported components
-import App from './App';
-import {LoremPara1, LoremPara2, AnotherApp} from './AnotherApp';
-import {BehindTheScenes} from './BehindTheScenes';
+import App from "./App";
+import { LoremPara1, LoremPara2, AnotherApp } from "./AnotherApp";
+import { BehindTheScenes } from "./BehindTheScenes";
+import JSXRules from "./JSXRules";
 
 // Note that Component identifier has Camel casing notion
 function MyFirstComponent() {
-  return <h1>Hello World</h1>;
+    return <h1>Hello World</h1>;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <MyFirstComponent />
-    <App />
-    <AnotherApp />
-    <LoremPara1 />
-    <LoremPara2 />
-    <BehindTheScenes />
-  </React.StrictMode>
+    <React.StrictMode>
+        <MyFirstComponent />
+        <App />
+        <AnotherApp />
+        <LoremPara1 />
+        <LoremPara2 />
+        <BehindTheScenes />
+        <JSXRules />
+    </React.StrictMode>
 );
-
