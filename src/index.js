7f80c30660427e3804b9ffe5e607ea0381a4ae0c
iamwatchdogs <shamith301102@gmail.com>
2023-12-04 21:48:31 +0530

    Created my First component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Note that Component identifier has Camel casing notion
+function MyFirstComponent() {
+  return <h1>Hello World</h1>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <MyFirstComponent />
   </React.StrictMode>
 );
 
