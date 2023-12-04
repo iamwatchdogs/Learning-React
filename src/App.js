117a6c275caca1dd5c367df5d14798a386102c09
iamwatchdogs <shamith301102@gmail.com>
2023-12-04 21:56:46 +0530

    Created two components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Creating an Header component.
+function Header() {
+  return <h2>This is imported Header element</h2>;
+}
+
+// returning JSX present within Header component using Header component.
+function App() {
+  return <Header />;
+}
+
+// exporting App component so that other files can use the component.
+// we are exporting it using keyword `default` as App is the name of file itself.
+export default App;
