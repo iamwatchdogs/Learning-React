fbb28173b98464d8455541a6626f2db39596f6b9
iamwatchdogs <shamith301102@gmail.com>
2023-12-04 22:18:07 +0530

    Added the imported components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Imported components
 import App from './App';
+import {LoremPara1, LoremPara2, AnotherApp} from './AnotherApp';
 
 // Note that Component identifier has Camel casing notion
 function MyFirstComponent() {
@@ -12,6 +15,9 @@ root.render(
   <React.StrictMode>
     <MyFirstComponent />
     <App />
+    <AnotherApp />
+    <LoremPara1 />
+    <LoremPara2 />
   </React.StrictMode>
 );
 
