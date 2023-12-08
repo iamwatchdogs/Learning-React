0991f492af2954077af5e63d4d7262fea2abeb28
iamwatchdogs <shamith301102@gmail.com>
2023-12-08 18:27:26 +0530

    Resetted to base state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-// Creating an Header component.
-function Header() {
-    return <h2>This is imported Header element</h2>;
+export default function App() {
+    return <></>;
 }
-
-// returning JSX present within Header component using Header component.
-function App() {
-    return <Header />;
-}
-
-// exporting App component so that other files can use the component.
-// we are exporting it using keyword `default` as App is the name of file itself.
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-// Imported components
-import App from "./App";
-import { LoremPara1, LoremPara2, AnotherApp } from "./AnotherApp";
-import { BehindTheScenes } from "./BehindTheScenes";
-import JSXRules from "./JSXRules";
-
-// Note that Component identifier has Camel casing notion
-function MyFirstComponent() {
-    return <h1>Hello World</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <MyFirstComponent />
         <App />
-        <AnotherApp />
-        <LoremPara1 />
-        <LoremPara2 />
-        <BehindTheScenes />
-        <JSXRules />
     </React.StrictMode>
 );
