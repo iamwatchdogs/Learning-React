35ae7f26d114b83ac98fcaf174b0554bd8db7975
iamwatchdogs <shamith301102@gmail.com>
2023-12-08 19:26:17 +0530

    Finished UsingDirectValues components

--- src/components/Expressions.jsx
+++ src/components/Expressions.jsx
@@ -39,10 +39,47 @@ const UsingVariables = () => {
     );
 };
 
+const UsingDirectValues = () => {
+    return (
+        <article className='using-direct-values'>
+            <section className='theory'>
+                <h2>Using Direct Values</h2>
+                <p>
+                    We can also use direct values in the jsx elements being
+                    returned. But for that we have to use curly braces for the
+                    jsx element to recoginize the elements as an expressions.
+                </p>
+            </section>
+            <section className='example'>
+                <h3>For example:</h3>
+                <ul>
+                    <li>
+                        <h4>Literal values:</h4>
+                        <ul>
+                            <li>String Literal: {"Hello World!"}</li>
+                            <li>String template: {`externalLorem[0:5]: ${externalLorem.substring(0, 5)}`}</li>
+                            <li>Numerical: {123}, {4.56}</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h4>Expressions:</h4>
+                        <ul>
+                            <li>{1} + {1} = {1 + 1}</li>
+                            <li>(9*5/3)%2 = {((9 * 5) / 3) % 2}</li>
+                            <li>{`'3' + 3 - 3`} = {"3" + 3 - 3}</li>
+                        </ul>
+                    </li>
+                </ul>
+            </section>
+        </article>
+    );
+};
+
 export default function Expressions() {
     return (
         <>
             <UsingVariables />
+            <UsingDirectValues />
         </>
     );
 }
