c2bae07ab2c06d01593b3580984833533188abf7
iamwatchdogs <shamith301102@gmail.com>
2023-12-10 14:42:57 +0530

    Imported Methods component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Expressions from "./components/Expressions";
+import Methods from "./components/Methods";
 
 export default function App() {
     return (
         <>
             <Expressions />
+            <Methods />
         </>
     );
 }
