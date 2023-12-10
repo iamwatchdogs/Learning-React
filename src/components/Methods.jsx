41efae5beaa845bf5bc8cbfa197bc5f15d34b30f
iamwatchdogs <shamith301102@gmail.com>
2023-12-10 14:49:24 +0530

    Wrote Introduction for Methods component

--- src/components/Methods.jsx
+++ src/components/Methods.jsx
@@ -1,3 +1,13 @@
-export default function Methods () {
-	return <></>;
+export default function Methods() {
+    return (
+        <article className='methods'>
+            <h1>Methods</h1>
+            <p>
+                We have discussed how to make function calls previously and here
+                we are going to take a look into some of most used methods for
+                accessing an object.
+            </p>
+			<br />
+        </article>
+    );
 }
