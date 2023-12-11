2768ec0bdc827f4d4937beee769d87967f14f1f4
iamwatchdogs <shamith301102@gmail.com>
2023-12-11 13:40:33 +0530

    Created ForEach component

--- src/components/Methods.jsx
+++ src/components/Methods.jsx
@@ -40,11 +40,67 @@ const Map = () => {
                 <p>
                     Please note that the map method take a call back method that
                     has two paramenter which are <b>item</b> and <b>key</b>.
-                    Even though <b>key</b> is optional, it is highly suggested to provide
-                    it as the key value is used to unqiuely identify one element from another.
-                    In case you have an ID attribute, then you can ignore write the call back
-                    function using key parameter, but you have make sure you use the id of
-                    the iterable for the generating jsx element.
+                    Even though <b>key</b> is optional, it is highly suggested
+                    to provide it as the key value is used to unqiuely identify
+                    one element from another. In case you have an ID attribute,
+                    then you can ignore write the call back function using key
+                    parameter, but you have make sure you use the id of the
+                    iterable for the generating jsx element.
+                </p>
+            </section>
+        </section>
+    );
+};
+
+const ForEach = () => {
+    const generateJsx = () => {
+        const elements = [];
+
+        // generating jsx elements to store them into an array
+        data.forEach((item) =>
+            elements.push(
+                <li key={item.id}>
+                    id: {item.id}, name: {item.name}, age: {item.age}
+                </li>
+            )
+        );
+
+        // returning all the jsx elements using `map()` method
+        return elements.map((item) => item);
+    };
+
+    return (
+        <section className='for-each-method'>
+            <section className='theory'>
+                <h2>For Each method</h2>
+                <p>
+                    For Each method is similar to map method but it does not
+                    return anything. It is used to iterate over an array of
+                    object and perform some action on each item of the object.
+                    For example, we can use this method to print the name of
+                    each person in the list.
+                </p>
+            </section>
+            <section className='example'>
+                <h3>For Example</h3>
+                <p>
+                    Here we are iterating over a list/array of object of some
+                    data that lists a group of people and contains data such as
+                    name, id, age.
+                </p>
+                <ul>{generateJsx()}</ul>
+            </section>
+            <section className='note'>
+                <h3>Note:</h3>
+                <p>
+                    Please note that the For Each method take a call back method
+                    that has two paramenter which are <b>item</b> and <b>key</b>
+                    . Even though <b>key</b> is optional, it is highly suggested
+                    to provide it as the key value is used to unqiuely identify
+                    one element from another. In case you have an ID attribute,
+                    then you can ignore write the call back function using key
+                    parameter, but you have make sure you use the id of the
+                    iterable for the generating jsx element.
                 </p>
             </section>
         </section>
@@ -54,16 +110,17 @@ const Map = () => {
 export default function Methods() {
     return (
         <article className='methods'>
+            <h1>Methods</h1>
             <section className='introduction'>
-                <h1>Methods</h1>
+                <h1>Introduction</h1>
                 <p>
                     We have discussed how to make function calls previously and
                     here we are going to take a look into some of most used
                     methods for accessing an object.
                 </p>
-                <br />
             </section>
             <Map />
+            <ForEach />
         </article>
     );
 }
