0f237b26ea004f7ba809354da026efd6212150ec
iamwatchdogs <shamith301102@gmail.com>
2024-02-15 20:19:40 +0530

    Added the examples to ImplementingProps components

--- src/components/ImplementingProps.jsx
+++ src/components/ImplementingProps.jsx
@@ -1,3 +1,6 @@
+import UserDisplayCardProps from "./examples/UserDisplayCardProps";
+import UserDisplayCardDestructuring from "./examples/UserDisplayCardDestructuring";
+
 export default function ImplementingProps() {
   return <></>;
 }
