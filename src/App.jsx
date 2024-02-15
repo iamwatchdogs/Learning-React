54d7efed8c22e6b0229ef22a1b43246e360218a8
iamwatchdogs <shamith301102@gmail.com>
2024-02-15 16:34:34 +0530

    Cleared previous section files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import Expressions from "./components/Expressions";
-import Methods from "./components/Methods";
-
 export default function App() {
-    return (
-        <>
-            <Expressions />
-            <Methods />
-        </>
-    );
+  return <></>;
 }
