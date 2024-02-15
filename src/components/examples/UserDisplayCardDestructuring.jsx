9025b18b7f73d5bf095ad1bddd03584037d68812
iamwatchdogs <shamith301102@gmail.com>
2024-02-15 21:00:38 +0530

    Finished the example components

--- src/components/examples/UserDisplayCardDestructuring.jsx
+++ src/components/examples/UserDisplayCardDestructuring.jsx
@@ -1,3 +1,11 @@
-export default function UserDisplayCardDestructuring() {
-  return <></>;
+export default function UserDisplayCardDestructuring({ user, age, phoneno, email }) {
+    return (
+      <div>
+        <h3>User Information:</h3>
+        <p>Name: {user}</p>
+        <p>Age: {age}</p>
+        <p>{phoneno}</p>
+        <p>Email Address: {email}</p>
+      </div>
+    );
 }
--- src/components/examples/UserDisplayCardProps.jsx
+++ src/components/examples/UserDisplayCardProps.jsx
@@ -1,3 +1,11 @@
-export default function UserDisplayCardProps() {
-  return <></>;
+export default function UserDisplayCardProps(props) {
+  return (
+    <div>
+      <h3>User Information:</h3>
+      <p>Name: {props.user}</p>
+      <p>Age: {props.age}</p>
+      <p>{props.phoneno}</p>
+      <p>Email Address: {props.email}</p>
+    </div>
+  );
 }
