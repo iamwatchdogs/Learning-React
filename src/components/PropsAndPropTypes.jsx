08bf0503a2335c6df9cd5443f115df3e5c435ab2
iamwatchdogs <shamith301102@gmail.com>
2024-02-15 18:54:50 +0530

    Finished writting header

--- src/components/PropsAndPropTypes.jsx
+++ src/components/PropsAndPropTypes.jsx
@@ -1,3 +1,14 @@
 export default function PropsAndPropTypes() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Props and PropTypes</h1>
+        <p>
+          Props and PropTypes are two core concepts in React. They are used to
+          define the shape of a React component. They play a key role in any React
+          application as they help to render the component based on the given data.
+        </p>
+      </header>
+    </>
+  );
 }
