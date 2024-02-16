33f43a9d0bcf1354e8105d8444bf8883f5128bb2
iamwatchdogs <shamith301102@gmail.com>
2024-02-16 22:13:42 +0530

    Created another example

---
+++ src/components/examples/UserDisplayCardBooleanProps.jsx
@@ -0,0 +1,18 @@
+export default function UserDisplayCardWithDefaultValues({
+  user = "Guest",
+  age = 18,
+  phoneno = "1234567890",
+  email = "guest@info.com",
+  isMarried
+}) {
+  return (
+    <div>
+      <h3>User Information:</h3>
+      <p>Name: {user}</p>
+      <p>Age: {age}</p>
+      <p>Phone Number: {phoneno}</p>
+      <p>Email Address: {email}</p>
+	  <p>Is Married: {isMarried ? "Yes" : "No"}</p>
+    </div>
+  );
+}
