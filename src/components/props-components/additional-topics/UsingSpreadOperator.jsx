4d4ab9a13eb46a3a7b8909d83dd4b1c5d33f9acd
iamwatchdogs <shamith301102@gmail.com>
2024-02-17 02:41:44 +0530

    Updated UsingSpreadOperator Component

--- src/components/props-components/additional-topics/UsingSpreadOperator.jsx
+++ src/components/props-components/additional-topics/UsingSpreadOperator.jsx
@@ -1,4 +1,5 @@
 import UserDisplayCardBooleanProps from "../../examples/UserDisplayCardBooleanProps";
+import UserDisplayCardWithSpreadOperator from "../../examples/UserDisplayCardWithSpreadOperator";
 
 function Title() {
   return <h3>Using Spread Operator</h3>;
@@ -104,12 +105,74 @@ function Example() {
   );
 }
 
+function AdditionalInformation() {
+  const sampleExample = `<UserDisplayCard gender="male" age={25} name="John Doe" />`;
+  const sampleComponent = `export default function UserDisplayCard({
+  user = "Guest",
+  age = 18,
+  phoneno = "1234567890",
+  email = "guest@info.com",
+  isMarried,
+  ...additionalData
+}) {
+  return (
+    <div>
+      <h3>User Information:</h3>
+      <p>Name: {user}</p>
+      <p>Age: {age}</p>
+      <p>Phone Number: {phoneno}</p>
+      <p>Email Address: {email}</p>
+      <p>Is Married: {isMarried ? "Yes" : "No"}</p>
+      {Object.keys(additionalData).map((key, id) => {
+        const value = additionalData[key];
+        return (
+          <p key={id}>
+            {key}: {value}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+`;
+
+  return (
+    <>
+      <p>
+        We can also use spread operator to overcome the disadvantage of
+        destructing at component receiving the data.
+      </p>
+      <p>
+        All we have to do is place a spread operator at the end of the
+        parameters that captures the additional key-value pairs.
+      </p>
+      <p>Let{"'"}s modify the above component to accept additional data:</p>
+      <pre>
+        <code>{sampleComponent}</code>
+      </pre>
+      <p>
+        Now, let{"'"}s test this out by creating a component and passing an
+        additional data of gender.
+      </p>
+      <pre>
+        <code>{sampleExample}</code>
+      </pre>
+      <UserDisplayCardWithSpreadOperator
+        gender='male'
+        age={25}
+        name='John Doe'
+      />
+    </>
+  );
+}
+
 export default function UsingSpreadOperator() {
   return (
     <>
       <Title />
       <Description />
       <Example />
+      <AdditionalInformation />
     </>
   );
 }
