0ee80e7a5d485c2bdfc8640e7f6efb4e232aa1fb
iamwatchdogs <shamith301102@gmail.com>
2024-02-21 15:13:59 +0530

    updated description

--- src/components/PropTypes.jsx
+++ src/components/PropTypes.jsx
@@ -12,6 +12,18 @@ function Desciption() {
         an external library till React v15.5.0. The PropTypes library is used as
         an default way to implement type checking into React-based application.
       </p>
+      <p>
+        PropTypes is an optional tool that is used for static type checking in
+        React. And most modern React application are not using PropTypes anymore
+        for typechecking, as most React-based application are built using
+        Typescript and it does a better job at Type Checking by letting you
+        define the type using <code>interface</code>.
+      </p>
+      <p>
+        It{"'"} suggest to type checking with Typescript rather than using
+        PropTypes, because PropTypes were only meant to act only as a
+        placeholder for type checking using Typescript or flow.
+      </p>
     </>
   );
 }
