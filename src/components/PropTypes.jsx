aa442702c3b67d9d1746560a75a7be23134db629
iamwatchdogs <shamith301102@gmail.com>
2024-02-21 13:47:09 +0530

    Added base title and description

--- src/components/PropTypes.jsx
+++ src/components/PropTypes.jsx
@@ -1,3 +1,24 @@
+function Header() {
+  return <h2>PropTypes</h2>;
+}
+
+function Desciption() {
+  return (
+    <>
+      <p>
+        PropTypes is one of default library of React library, that was initially
+        an external library till React v15.5.0. The PropTypes library is used as
+        an default way to implement type checking into React-based application.
+      </p>
+    </>
+  );
+}
+
 export default function PropTypes() {
-	return <></>;
+  return (
+    <>
+      <Header />
+      <Desciption />
+    </>
+  );
 }
