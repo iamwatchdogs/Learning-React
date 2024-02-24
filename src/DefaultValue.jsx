47bec86abde78e5b33a4e149b37b38fe18600495
iamwatchdogs <shamith301102@gmail.com>
2024-02-24 15:10:34 +0530

    Finished Description in DefaultValues

--- src/DefaultValue.jsx
+++ src/DefaultValue.jsx
@@ -2,6 +2,35 @@ import React from "react";
 
 export default class DefaultValue extends React.Component {
   render() {
-    return <></>;
+    const Descripition = () => (
+      <>
+        <p>
+          And unlike the functional components, it{"'"}s not that easy to set
+          the default prop values since we can{"'"}t access them directly.
+        </p>
+        <p>
+          You can set default values for props in a class component in two ways:
+        </p>
+        <ul>
+          <li>
+            By defining a <code>static</code> property called as{" "}
+            <code>defaultProps</code> within the class.
+          </li>
+          <li>
+            By appendin the <code>defaultProps</code> attribute to the class
+            component after the class compoent has been defined.{" "}
+            <i>
+              (Just like <code>PropTypes</code>)
+            </i>
+          </li>
+        </ul>
+      </>
+    );
+    
+    return (
+      <>
+        <Descripition />
+      </>
+    );
   }
 }
