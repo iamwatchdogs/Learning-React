600d5d37492fac74457e74e9dd00e32aaf74fd56
iamwatchdogs <shamith301102@gmail.com>
2024-02-24 15:06:04 +0530

    Created DefaultValues

--- src/PropsAndPropTypesInClassComponents.jsx
+++ src/PropsAndPropTypesInClassComponents.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Introduction from "./Introduction";
+import DefaultValue from "./DefaultValue";
 
 export default class PropsAndPropTypesInClassComponents extends React.Component {
   render() {
     return (
       <>
         <Introduction />
+        <DefaultValue />
       </>
     );
   }
